87ce1b6062b00a5b0cf2c12d04fca80dfdf653de
Oh,Seunghoon <stdsalmon@gmail.com>
2019-01-22 11:24:43 +0900

    Fix : stamp holder, stamp item xml

--- app/src/main/java/com/tistory/deque/previewmaker/kotlin/main/KtStampHolder.kt
+++ app/src/main/java/com/tistory/deque/previewmaker/kotlin/main/KtStampHolder.kt
@@ -5,17 +5,22 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.tistory.deque.previewmaker.R
 import com.tistory.deque.previewmaker.kotlin.model.Stamp
+import kotlinx.android.synthetic.main.kt_stamp_list_item.view.*
 import kotlinx.android.synthetic.main.list_item.view.*
 
-class KtStampHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+class KtStampHolder(parent: ViewGroup, val clickStamp:(Stamp)->Unit, val delStamp:(Stamp)->Unit ) : RecyclerView.ViewHolder(
         LayoutInflater
                 .from(parent.context)
-                .inflate(R.layout.list_item, parent, false)
+                .inflate(R.layout.kt_stamp_list_item, parent, false)
 ) {
     fun onBind(stampItem: Stamp) {
         itemView.run {
-            stampListImageView.run { post { setImageURI(stampItem.imageUri) } }
-            stampListTextView.run { post { text = stampItem.name } }
+            stamp_item_image_view.run { post { setImageURI(stampItem.imageUri) } }
+            stamp_item_name_text_view.run { post { text = stampItem.name } }
+
+            stamp_item_select_layout.setOnClickListener {
+                clickStamp(stampItem)
+            }
         }
     }
 }
